d9f7ea7486cdf3f7589d05ccb0d7e7385e96259b
MuraliJbk <murali@jbkinfotech.com>
2023-09-12 16:54:39 +0530

    marketing cta

--- js/modules/gsap.js
+++ js/modules/gsap.js
@@ -1,13 +1,47 @@
 import { gsap } from "gsap";
 import { CSSPlugin } from "gsap/CSSPlugin";
-
 gsap.registerPlugin(CSSPlugin);
 
-// Now you can use gsap to create animations
-const photo = document.getElementById("photo");
-
-gsap.to(photo, {
-  duration: 2,
-  width: "200px",
-  height: "150px",
+// scroller polygon icon
+let scroller = jQuery(".scroller_icon");
+scroller.each(function(){
+    const _self = jQuery(this);
+    const _path = _self.find('svg path');
+    const start = "M41 52.5L10 2.5L72 2.5L41 52.5Z";
+    const end = "M41 53.5L10 3.5L72 4.5L41 53.5Z";
+    gsap.timeline({ 
+        repeat: -1,
+        repeatDelay: 1,
+    }).fromTo(_path[0], { y: -8, attr: { d: start }}, { y: 8, attr: { d: end }, duration: 2, ease: 'bounce.out'})
 });
+
+// scroller polygon icon
+let btnSvg = jQuery(".btn-link");
+btnSvg.each(function(){
+    const _self = jQuery(this);
+    const _path = _self.find('svg path');
+    const start = "M41 3L36 0.113249V5.88675L41 3ZM0 3.5H36.5V2.5H0V3.5Z";
+    const end = "M41 3L26 0.113249V5.88675L41 3ZM0 3.5H36.5V2.5H0V3.5Z";
+    gsap.timeline({ 
+        repeat: -1,
+        repeatDelay: 1,
+    }).fromTo(_path[0], { x: -100, attr: { d1: start }}, { x: 0, attr: { d1: end }, duration: 1, ease: 'power1.out'})
+});
+
+// data-animation
+var gsap_elements = jQuery('[data-animation]');
+gsap_elements.each(function() {
+    const _gself = jQuery(this);
+    const _gclass = _gself.data('animation');
+    if(_gclass === 'gsap-left') {
+        var tl = gsap.timeline({ paused: true })
+        tl.fromTo(_gself[0], { x: -100, transition: 'none'}, { x: 0, duration: 2, ease: 'power1.out'})
+        _gself[0].tl = tl
+    }
+    if(_gclass === 'gsap-right') {
+        var tl = gsap.timeline({ paused: true })
+        tl.fromTo(_gself[0], { x: 100, transition: 'none'}, { x: 0, duration: 2, ease: 'power1.out'})
+        _gself[0].tl = tl
+    }
+});
+
