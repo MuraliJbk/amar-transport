70ce27bc83f93806bf99b12131fce0697175b825
MuraliJbk <murali@jbkinfotech.com>
2023-09-14 12:07:26 +0530

    test

--- js/modules/swiper.js
+++ js/modules/swiper.js
@@ -1,43 +1,57 @@
 import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+import {FreeMode, Navigation, Pagination, Thumbs} from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import 'swiper/css/thumbs';
+Swiper.use([FreeMode,Navigation, Pagination, Thumbs]);
 
-var $window = jQuery(window);
-const introSwiper = new Swiper('.short-intro-swiper', {
-  modules: [Navigation, Pagination],
-    direction: 'horizontal',
-    slidesPerView: 'auto',
+jQuery(document).ready(function($){
+  var $window = $(window);
+  const introSwiper = new Swiper('.short-intro-swiper', {
+    modules: [Navigation, Pagination],
+      direction: 'horizontal',
+      slidesPerView: 'auto',
+      effect: 'slide',
+      speed: 1000,
+      pagination: {
+        el: '.intro-pagination',
+        clickable: true,
+      },
+  });
+  const toggleSwiper = () => { $(window).width() >= 1024 ? introSwiper.destroy() : introSwiper.enable(); }
+  $window.on('resize load', toggleSwiper);
+  $window.trigger('resize load', toggleSwiper);
+
+  const newsSwiperNav = new Swiper(".news-swiper-nav", {
+    modules: [Navigation, Pagination, FreeMode],
+    direction: 'vertical',
+    slidesPerView: 1,
+    slidesPerScroll: 1,
     effect: 'slide',
-		speed: 1000,
+    speed: 1000,
+    FreeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
     pagination: {
-      el: '.intro-pagination',
+      el: '.news-swiper-pagination',
       clickable: true,
     },
-});
-const toggleSwiper = () => { jQuery(window).width() >= 1024 ? introSwiper.destroy() : introSwiper.enable(); }
-$window.on('resize load', toggleSwiper);
-$window.trigger('resize load', toggleSwiper);
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  const newsSwiperFor = new Swiper(".news-swiper-for", {
+    modules: [Thumbs],
+    direction: 'horizontal',
+    slidesPerView: 1,
+    slidesPerScroll: 1,
+    effect: 'slide',
+    speed: 1000,
+    thumbs: {
+      swiper: newsSwiperNav
+    }
+  });
 
-const newsSwiperFor = new Swiper(".news-swiper-for", {
-  direction: 'horizontal',
-  slidesPerView: 1,
-  effect: 'slide',
-  speed: 1000,
-  
-});
-const newsSwiperNav = new Swiper(".news-swiper-nav", {
-  direction: 'vertical',
-  slidesPerView: 3,
-  effect: 'slide',
-  speed: 1000,
-  pagination: {
-    el: '.news-swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
 });
