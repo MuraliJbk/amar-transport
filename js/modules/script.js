c583990a92041db14273ad4e3152e7d47a4ac410
MuraliJbk <murali@jbkinfotech.com>
2023-09-16 10:51:24 +0530

    test

--- js/modules/script.js
+++ js/modules/script.js
@@ -6,6 +6,16 @@ jQuery(document).ready(function($){
         lastScrollTop = scroll;
     });
 
+    $('.accordion-header').on('click', function(e){
+        e.preventDefault();
+        $(this).parent().toggleClass('active');
+        $(this).parent().siblings().removeClass('active');
+        $(this).parent().siblings().find('.accordion-header').removeClass('open');
+        $(this).toggleClass("open");
+        $(this).siblings('.accordion-content').slideToggle(500);
+        $(this).parent().siblings().find('.accordion-content').slideUp(500);
+    });
+    
     if($(window).width() >= 768){
         $(".leadership-list").each(function() {
             let _this = $(this).children('.leadership-text');
