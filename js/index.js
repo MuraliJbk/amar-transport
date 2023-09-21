c1165dc134c950a541639c10797e6561e74e148f
MuraliJbk <murali@jbkinfotech.com>
2023-09-21 21:26:43 +0530

    text

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 
+import './lib/jquery.selectBox';
+import './lib/slick.min';
 import './modules/animation';
 import './modules/counter';
 import './modules/script';
 import './modules/gsap';
 import './modules/effects';
-import './lib/jquery.selectBox';
-import './modules/selectBox';
 import './modules/magnific-popup';
+import './modules/selectBox';
 import './modules/slick';
--- js/modules/slick.js
+++ js/modules/slick.js
@@ -1,15 +1,30 @@
-import jQuery from 'jquery';
-import slick from 'slick-carousel';
-import 'slick-carousel/slick/slick.css';
-
-
-(function($) {
+jQuery(document).ready(function($){
     $('.client-logos-slider').slick({
         slidesToShow: 8,
         slidesToScroll: 1,
         arrows: false,
         dots: false,
         autoplay: true,
+      });
+
+      $('.news-slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        asNavFor: '.news-slider-nav'
+      });
+      $('.news-slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        vertical: true,
+        verticalSwiping: true,
+        asNavFor: '.news-slider-for',
+        focusOnSelect: true,
+        dots: true,
+        arrows: true,
+        prevArrow: '<div class="slick-arrow slick-prev button flex flex-center"><span class="fa-sharp fa-solid fa-arrow-left"></span></div>',
+        nextArrow: '<div class="slick-arrow slick-next button flex flex-center"><span class="fa-sharp fa-solid fa-arrow-right"></span></div>',
       });
 
     $('.other-services-slider').slick({
@@ -38,7 +53,7 @@ import 'slick-carousel/slick/slick.css';
     dots: false,
   });
  
-})(jQuery);
+});
  
 
 
