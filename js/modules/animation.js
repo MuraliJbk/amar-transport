bccadf84d5948bc475f6a55a1f3d4051d6305f9f
MuraliJbk <murali@jbkinfotech.com>
2023-09-26 18:19:42 +0530

    test

--- js/modules/animation.js
+++ js/modules/animation.js
@@ -1,25 +1,29 @@
 
-import 'is-in-viewport';
-import jQuery from 'jquery';
 var $animation_elements = jQuery('[data-animation]');
 var $window = jQuery(window);
+var $document = jQuery(document);
 
 function check_if_in_view() {
+  let window_height = $window.height()
+  let window_top_position = $window.scrollTop();
+  let window_bottom_position = window_top_position + window_height;
   $animation_elements.each(function() {
     const $self = jQuery(this);
     const animation = $self.data('animation');
-    const animateType = $self.data('animate');
-    const delay = Number($self.data('animation-delay') || 0);
+    let element_height = $self.outerHeight();
+    let element_top_position = $self.offset().top;
+    let element_bottom_position = element_top_position + element_height;
+    
     const timeline = $self[0].tl
     const counter = $self[0].counter
-    if($self.is(':in-viewport')) {
-      setTimeout(() => {
-        if (animateType) _.animateRun($self, animateType);
-        else $self.addClass('visible ' + animation);
+      if (
+      (element_bottom_position >= window_top_position) &&
+      (element_top_position <= window_bottom_position)
+    ) {
+        $self.addClass('visible ' + animation);
         if (timeline) {
           timeline.play();
         }
-      }, delay);
     } else {
       if (timeline && timeline.progress() > 0) {
         timeline.pause();
@@ -30,5 +34,10 @@ function check_if_in_view() {
     }
   });
 }
-$window.on('scroll resize load', check_if_in_view);
-$window.trigger('scroll resize load', check_if_in_view);
+
+$window.on("scroll", check_if_in_view);
+$window.trigger("scroll");
+$document.ready(function(){ 
+  check_if_in_view(); 
+  setTimeout(check_if_in_view, 100);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -17,19 +17,7 @@ module.exports = {
         },
       },
       {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]', // This is the default value
-              outputPath: 'fonts/', // You can customize the output path
-            },
-          },
-        ],
-      },
-      {
-        test: /\.css$/, // This rule matches .css files
+        test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
     ],
