40801fe00be93c6a93fcaea45eb0fb65b9811d6c
MuraliJbk <murali@jbkinfotech.com>
2023-10-05 10:44:09 +0530

    test

--- js/modules/gsap.js
+++ js/modules/gsap.js
@@ -1,7 +1,5 @@
-import { gsap } from "gsap";
-
-var DrawSVGPlugin = DrawSVGPlugin || window.DrawSVGPlugin 
-var CountUp = CountUp || window.CountUp 
+import { gsap} from "gsap";
+import {DrawSVGPlugin} from './DrawSVGPlugin.min.js'
 gsap.registerPlugin(DrawSVGPlugin)
 
 // scroller polygon icon
@@ -19,44 +17,38 @@ scroller.each(function(){
 
 // data-animation
 var gsap_elements = jQuery('[data-animation]');
-gsap_elements.each(function() {
+gsap_elements.each(function () {
     const _gself = jQuery(this);
     const _gclass = _gself.data('animation');
-    if(_gclass === 'gsap-left') {
-        var tl= gsap.timeline({ paused: true })
-        tl.fromTo(_gself[0], { x: -50, transition: 'none'}, { x: 0, duration: 1.5, ease: 'power1.out'})
-        _gself[0].tl = tl
+    var tl = gsap.timeline({ paused: true });
+    switch (_gclass) {
+        case 'gsap-left':
+            tl.fromTo(_gself[0], { x: -50, transition: 'none' }, { x: 0, duration: 1.5, ease: 'power1.out' });
+            break;
+        case 'gsap-right':
+            tl.fromTo(_gself[0], { x: 50, transition: 'none' }, { x: 0, duration: 1.5, ease: 'power1.out' });
+            break;
+        case 'gsap-up':
+            tl.fromTo(_gself[0], { y: 25, transition: 'none' }, { y: 0, duration: 1.5, ease: 'power1.out' });
+            break;
+        case 'gsap-down':
+            tl.fromTo(_gself[0], { y: -25, transition: 'none' }, { y: 0, duration: 1.5, ease: 'power1.out' });
+            break;
+        default: break;
     }
-    if(_gclass === 'gsap-right') {
-        var tl = gsap.timeline({ paused: true })
-        tl.fromTo(_gself[0], { x: 50, transition: 'none'}, { x: 0, duration: 1.5, ease: 'power1.out'})
-        _gself[0].tl = tl
-    }
-    if(_gclass === 'gsap-up') {
-        var tl = gsap.timeline({ paused: true })
-        tl.fromTo(_gself[0], { y: 25, transition: 'none'}, { y: 0, duration: 1.5, ease: 'power1.out'})
-        _gself[0].tl = tl
-    }
-    if(_gclass === 'gsap-down') {
-        var tl = gsap.timeline({ paused: true })
-        tl.fromTo(_gself[0], { y: -25, transition: 'none'}, { y: 0, duration: 1.5, ease: 'power1.out'})
-        _gself[0].tl = tl
-    }
+    _gself[0].tl = tl;
 });
+
 var _ui = jQuery("[data-animation='ui-ele']");
-console.log(_ui)
 _ui.each(function(){
- var $self = jQuery(this);
- var _paths = $self.find("mask path");
- var isAsync = $self.attr('data-animation-async')
- var isLong = 0.25
- var tl = gsap.timeline({
-    paused: true,
-    delay: 0.25,
- })
- _paths.each(function(i){
-    tl.fromTo(jQuery(this)[0], { drawSVG: '0%' }, { drawSVG: '100%', duration: isLong ? 2 : 1, ease: 'power1.out' }, i === 0 ? '<' : isAsync ? '>-0.5' : '>-0.75')
- })
- $self[0].tl = tl
+    var $self = jQuery(this);
+    var _paths = $self.find("mask path");
+    var isAsync = $self.attr('data-animation-async')
+    var isLong = 0.25
+    var tl = gsap.timeline({ paused: true, delay: 0.25, })
+    _paths.each(function(i){
+        tl.fromTo(jQuery(this)[0], { drawSVG: '0%' }, { drawSVG: '100%', duration: isLong ? 2 : 1, ease: 'power1.out' }, i === 0 ? '<' : isAsync ? '>-0.5' : '>-0.75')
+    })
+    $self[0].tl = tl
 });
 
