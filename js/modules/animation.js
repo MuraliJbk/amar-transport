a0f02c38b911a1e36bae395845dacc6d98958b16
MuraliJbk <murali@jbkinfotech.com>
2023-10-18 09:56:25 +0530

    test

--- js/modules/animation.js
+++ js/modules/animation.js
@@ -1,12 +1,13 @@
-
 var $animation_elements = jQuery('[data-animation]');
 var $window = jQuery(window);
 var $document = jQuery(document);
 
 function check_if_in_view() {
-  let window_height = $window.height()
+  let window_height = $window.height();
   let window_top_position = $window.scrollTop();
   let window_bottom_position = window_top_position + window_height;
+  let viewport_threshold = 0.25; // Adjust this to change the threshold
+
   $animation_elements.each(function() {
     const $self = jQuery(this);
     let element_height = $self.outerHeight();
@@ -14,19 +15,26 @@ function check_if_in_view() {
     let element_bottom_position = element_top_position + element_height;
     
     const animation = $self.data('animation');
-    const timeline = $self[0].tl
-    const counter = $self[0].counter
+    const timeline = $self[0].tl;
+    const counter = $self[0].counter;
 
-    if((element_bottom_position >= window_top_position) && (element_top_position <= window_bottom_position) ) {
-        $self.addClass('visible ' + animation);
-        if(timeline) { timeline.play(); }
+    // Calculate the threshold for an element to be considered in view
+    let threshold = element_height * viewport_threshold;
+
+    if ((element_top_position + threshold <= window_bottom_position) && (element_bottom_position >= window_top_position + threshold)) {
+      $self.addClass('visible ' + animation);
+      if (timeline) { timeline.play(); }
     } 
-    else{
-      if(timeline && timeline.progress() > 0) { timeline.pause(true); }
-      if(counter) { counter.reset(); }
+    else {
+      if (timeline && timeline.progress() > 0) { timeline.pause(true); }
+      if (counter) { counter.reset(); }
     }
   });
 }
+
 $window.on("scroll", check_if_in_view);
 $window.trigger("scroll");
-$document.ready(function(){ check_if_in_view(); setTimeout(check_if_in_view, 100); });
+$document.ready(function() { 
+  check_if_in_view();
+  setTimeout(check_if_in_view, 100);
+});
