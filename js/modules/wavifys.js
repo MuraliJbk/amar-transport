fdfc2a7501782dc505b16838fc8591535f3bae29
MuraliJbk <murali@jbkinfotech.com>
2023-10-19 11:25:08 +0530

    test

--- js/modules/wavifys.js
+++ js/modules/wavifys.js
@@ -1,13 +1,12 @@
-
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function(){
   const myWave = document.querySelectorAll('.seaport-wave');
-  myWave.forEach(function(wave, index) {
-    wave.wavify({
+  myWave.forEach(function(wave, index){
+    wavify(wave, {
       height: 40,
       bones: 40,
       amplitude: 20,
       color: '#E9F6FC',
       speed: 0.25,
     });
-  });
+  })
 });
